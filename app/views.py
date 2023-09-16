8e6befa941b0b9a50f393c038b9c7118d78397a3
mahesh <boyamahesh8039@gmail.com>
2023-09-16 11:46:03 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def Ip(request,data):
+    return HttpResponse('Hai Hello How Are You Python')
